dedff85d84062d03948acc3be66ffe5e50b1da53
Calum Murray <Cal_92@hotmail.co.uk>
2013-09-15 11:56:58 +0100

    Added empty JUnit Test Project

---
+++ JobSeekerServer/src/test/java/ServerTestSuite.java
@@ -0,0 +1,27 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ *
+ * @author Calum
+ */
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class ServerTestSuite 
+{
+
+    @Before
+    public void setUp() throws Exception 
+    {
+       
+    }
+
+    @After
+    public void tearDown() throws Exception 
+    {
+    }
+    
+}
